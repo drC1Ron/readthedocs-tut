bd8b38d6b0dd551ce8c6f04ea99524fc45f6397a
Ronny Landsverk <47351038+drC1Ron@users.noreply.github.com>
2023-07-13 11:43:46 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'drC1Ron-rtd-tutorial'
+copyright = '2023 - drC1Ron'
+author = 'drC1Ron'
 
 release = '0.1'
 version = '0.1.0'
